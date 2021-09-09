23becdcef210d9c91b526c873feebe27387b46fe
Xiao Lei Zhang <zhang205@yorku.ca>
2021-09-09 17:26:59 -0400

    Implemented get_fcn_resnet50_model_instance in train_fcn.py.

--- train_fcn.py
+++ train_fcn.py
@@ -7,6 +7,8 @@ import torch.utils.data
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.models.segmentation import fcn_resnet50
+from torchvision.models.segmentation import fcn_resnet101
 
 from data.apple_dataset import AppleDataset
 from utility.engine import train_one_epoch, evaluate
@@ -19,6 +21,12 @@ import utility.transforms as T
 # using the MinneApple dataset
 ######################################################
 
+model_urls = {
+    'fcn_resnet50_coco': 'https://download.pytorch.org/models/fcn_resnet50_coco-1167a1af.pth',
+    'fcn_resnet101_coco': 'https://download.pytorch.org/models/fcn_resnet101_coco-7ecb50ca.pth',
+    'deeplabv3_resnet50_coco': 'https://download.pytorch.org/models/deeplabv3_resnet50_coco-cd0a2569.pth',
+    'deeplabv3_resnet101_coco': 'https://download.pytorch.org/models/deeplabv3_resnet101_coco-586e9e4e.pth',
+}
 
 def get_transform(train):
     transforms = []
@@ -27,6 +35,59 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
+def get_fcn_resnet50_model_instance(num_classes):
+    """
+    This method gets an instance of FCN-Resnet-50 model given num_classes.
+    Model is pretrained on a subset of COCO train2017, on the 20 categories
+    that are present in the Pascal VOC dataset. Output layer of model
+    uses the first num_classes pretrained filters. 0 < num_classes <= 21
+
+    :param num_classes: number of classes
+    :return: instance of FCN-Resnet-50 model.
+    """
+
+    # Checks constraint on num_classes
+    if num_classes <= 0 or num_classes > 21:
+        raise NotImplementedError('num_classes={} is out of range.'.format(num_classes))
+
+    model = fcn_resnet50(pretrained=False, num_classes=num_classes)
+
+    arch_type = 'fcn'
+    backbone = 'resnet50'
+
+    # Loads pretrained model with num_classes number of classes
+    arch = arch_type + '_' + backbone + '_coco'
+    model_url = model_urls[arch]
+    if model_url is None:
+        raise NotImplementedError('pretrained {} is not supported as of now'.format(arch))
+    else:
+
+        # Loads state dictionary of pretrained model
+        state_dict = torch.hub.load_state_dict_from_url(model_url, progress=True)
+
+        # gets state dict of model
+        state_dict_self = model.state_dict()
+        for i, (name, param) in enumerate(state_dict_self.items()):
+            if name in ['classifier.4.weight', 'classifier.4.bias']:
+                state_dict_self[name].copy_(state_dict[name][:num_classes])
+                # print(torch.all(model.state_dict()[name] == state_dict[name][:num_classes]))
+            else:
+                state_dict_self[name].copy_(state_dict[name])
+                # print(torch.all(model.state_dict()[name] == state_dict[name]))
+    return model
+
+def get_fcn_resnet101_model_instance(num_classes):
+    """
+    This method gets an instance of FCN-Resnet-101 model given num_classes.
+    Model is pretrained on a subset of COCO train2017, on the 20 categories
+    that are present in the Pascal VOC dataset. Output layer of model
+    uses the first num_classes pretrained filters. 0 < num_classes <= 21
+
+    :param num_classes: number of classes
+    :return: instance of FCN-Resnet-101 model.
+    """
+    model = fcn_resnet101(pretrained=False, num_classes=num_classes)
+    return model
 
 def get_maskrcnn_model_instance(num_classes):
     # load an instance segmentation model pre-trained pre-trained on COCO
@@ -123,7 +184,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch Detection Training')
     parser.add_argument('--data_path', default='/home/nicolai/phd/data/cvppp_2019/apple_dataset', help='dataset')
     parser.add_argument('--dataset', default='AppleDataset', help='dataset')
-    parser.add_argument('--model', default='maskrcnn', help='model')
+    parser.add_argument('--model', default='maskrcnn', help='model: fcn_resnet50, fcn_resnet101')
     parser.add_argument('--device', default='cuda', help='device')
     parser.add_argument('-b', '--batch-size', default=2, type=int)
     parser.add_argument('--epochs', default=13, type=int, metavar='N', help='number of total epochs to run')
@@ -140,7 +201,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     print(args.model)
-    assert(args.model in ['mrcnn', 'frcnn'])
+    assert(args.model in ['fcn_resnet50', 'fcn_resnet101'])
 
     if args.output_dir:
         utils.mkdir(args.output_dir)
