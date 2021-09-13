648a27c75cd78c5e177d254e686860fc6683533e
Xiao Lei Zhang <zhang205@yorku.ca>
2021-09-12 22:44:06 -0400

    Added support for horizontal flip transform for case in which target masks are not available. Added some comments to transforms.py.

--- utility/transforms.py
+++ utility/transforms.py
@@ -15,38 +15,90 @@ def _flip_coco_person_keypoints(kps, width):
 
 
 class Compose(object):
+    """
+    This class defines a composition of transforms.
+    """
     def __init__(self, transforms):
+        """
+        This method initializes an instance of Compose given
+        list of transforms transforms
+        :param transforms: list of transforms.
+        """
         self.transforms = transforms
 
     def __call__(self, image, target):
+        """
+        This method applies each transform in self.transforms sequentially to image and
+        target given image and target.
+        :param image: image
+        :param target: target
+        :return: image, target after applying each transform in self.transforms
+        """
         for t in self.transforms:
             image, target = t(image, target)
         return image, target
 
 
 class RandomHorizontalFlip(object):
+    """
+    This class defines a Random Horizontal Flip transform.
+    Image or target data are flipped horizontally with some probability prob.
+    """
     def __init__(self, prob):
+        """
+        This method initializes an instance of RandomHorizontalFlip transform
+        given prob.
+        :param prob: probability of horizontal flip
+        """
         self.prob = prob
 
     def __call__(self, image, target):
-        if random.random() < self.prob and "masks" in target and target["masks"] is not None:
-            height, width = image.shape[-2:]
-            image = image.flip(-1)
-            bbox = target["boxes"]
-            bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
-            target["boxes"] = bbox
-            if "masks" in target:
-                target["masks"] = target["masks"].flip(-1)
-            if "keypoints" in target:
-                keypoints = target["keypoints"]
-                keypoints = _flip_coco_person_keypoints(keypoints, width)
-                target["keypoints"] = keypoints
+        """
+        This method performs random horizontal flip transform on image
+        and target with probability self.prob given image and target.
+        If a horizontal flip is to be performed and the target masks are
+        not available, then only the image is flipped.
+        :param image: image
+        :param target: target
+        :return: image, target
+        """
+        if random.random() < self.prob:
+
+            # Flips image and GT data, if masks are available.
+            # Flips image, otherwise.
+            if "masks" in target and target["masks"] is not None:
+                height, width = image.shape[-2:]
+                image = image.flip(-1)
+                bbox = target["boxes"]
+                bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
+                target["boxes"] = bbox
+                if "masks" in target:
+                    target["masks"] = target["masks"].flip(-1)
+                if "keypoints" in target:
+                    keypoints = target["keypoints"]
+                    keypoints = _flip_coco_person_keypoints(keypoints, width)
+                    target["keypoints"] = keypoints
+            else:
+                image = image.flip(-1)
 
         return image, target
 
 
 class ToTensor(object):
+    """
+    This class defines a ToTensor transform.
+    ToTensor transform converts image to PyTorch Tensor
+    and normalizes the image to [0,1] range.
+    """
     def __call__(self, image, target):
+        """
+        This method applies ToTensor transform to image given
+        image and target. ToTensor transform converts image to
+        Pytorch Tensor and normalizes image to [0,1] range.
+        :param image: image
+        :param target: target
+        :return: image, target
+        """
         image = F.to_tensor(image)
         return image, target
 
